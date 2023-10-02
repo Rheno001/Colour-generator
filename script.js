cf6ce2594c397d4e3ad8d7a2cc6a8d3b129e9061
--replace-all <renfredreuben@gmail.com>
2023-10-02 02:45:13 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const button = documnet.getElementById('btn');
-const body =document.querySelector('body');
+const button = document.getElementById('btn');
+const body = document.querySelector('body');
 const color = ['red', 'green', 'blue', 'yellow', 'pink', 'grey', 'lime'];
 
 body.style.backgroundColor = 'black';
 button.addEventListener('click', () => {
     const colorIndex = parseInt(Math.random() * color.length);
+    body.style.backgroundColor = color[colorIndex];
+    body.style.transition = '1s linear';
 });
